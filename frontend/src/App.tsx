d4debac278ed04141c83e5346b6a3ad72df4533d
HarshaJayaweera21 <harshajayaweera1965@gmail.com>
2026-02-25 11:03:33 +0530

    add Header component and update routing for user and admin management

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import VerifyEmail from './pages/VerifyEmail';
 import AdminDashboard from './pages/AdminDashboard';
+import Dashboard from './pages/Dashboard';
+import ManageUsers from './pages/ManageUsers';
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
 
@@ -13,6 +15,8 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/verify-email" element={<VerifyEmail />} />
       <Route path="/admin-dashboard" element={<AdminDashboard />} />
+      <Route path="/admin/manage-users" element={<ManageUsers />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/reset-password" element={<ResetPassword />} />
       {/* Redirect root to login for now */}
