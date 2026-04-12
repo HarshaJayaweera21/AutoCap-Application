5de719db33ae2eb8f00a327b17c209eba4fd7a99
Sandaruwan Shanaka <perolopessthesoosa@gmail.com>
2026-04-12 15:59:52 +0530

    Revert "feat: implement theme context and provider for light/dark mode support"
    This reverts commit 204a716dfc83965a6f52a5d25081306a7a8a5d5b.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ThemeProvider } from './context/ThemeContext.tsx'
 import './index.css'
 import App from './App.tsx'
 
@@ -11,11 +10,9 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
 )
